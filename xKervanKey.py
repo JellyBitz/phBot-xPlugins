cb6ff0b36a0abe76243f54fea6184191c980e10b
JellyBitz <engelsquintero@gmail.com>
2019-05-23 00:55:04 -0300

    Added version for JOYSRO

--- xKervanKey.py
+++ xKervanKey.py
@@ -2,15 +2,18 @@ from phBot import *
 import phBotChat
 import QtBind
 import os
-from time import sleep
+from threading import Timer
 import urllib.request
+
+pName = 'xKervanKey'
+pVersion = '0.1.0'
 
 # SET YOUR CUSTOM "xkervankey.txt" PATH HERE.
 # xkervankey_path = r"C:\Users\MyUser\Downloads\phBot\Plugins\xkervankey.txt" # EXAMPLE
 xkervankey_path = ""
 
 # Initializing GUI
-gui = QtBind.init(__name__,'xKervanKey')
+gui = QtBind.init(__name__,pName)
 lblKey = QtBind.createLabel(gui,"KervanKey:",21,11)
 tbxKey = QtBind.createLineEdit(gui,"",130,12,150,18)
 btnKey = QtBind.createButton(gui, "btnKey_clicked","Save",300,11)
@@ -58,25 +61,26 @@ def getKervanKey():
 
 # Load website to extract the key
 def loadKervanKeyFromSite():
-	# Emulating a quick browser to open google docs
-	req = urllib.request.Request("https://docs.google.com/spreadsheets/d/1xhbP2qcBLscZ7j8D14QqkS44MU1kEkOQZqh24kwdfXQ/export?format=csv", headers={'User-Agent' : "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0"})
+	# Emulating a quick browser to open the site
+	req = urllib.request.Request("https://www.joysro.net/code.php", headers={'User-Agent' : "Mozilla/5.0 (Windows NT 6.2; WOW64; rv:22.0) Gecko/20100101 Firefox/22.0"})
 	# Getting stream
 	resp = urllib.request.urlopen(req)
 	# Getting data
 	html = str(resp.read().decode("utf-8"))
-	# Extracting data from excel (CSV format)
-	index = html.find(',RSilkroad - KervanKey :,,,,')
+	# Extracting data
+	searchingStart = '<h1>'
+	searchingEnd = '</h1>'
+	index = html.find(searchingStart)
 	if index != -1:
-		return html[index+28:].split(",")[0]
+		return html[index+len(searchingStart):].split(searchingEnd)[0]
 	return ""
 
 # All chat messages received are sent to this function
 def handle_chat(t, player, msg):
 	if player:
-		if player == "BotCheck" and "BotCheck" in msg:
-			sleep(1.0)
-			phBotChat.Private(player,getKervanKey())
+		if player == "JOYCONTROL" and "joysro.net" in msg:
+			Timer(1.0,phBotChat.Private,(player,getKervanKey())).start()
 
 # Success at loading
-log("Plugin: xKervanKey v0.0.10 successfully loaded.")
+log("Plugin: "+pName+" v"+pVersion+" [JOYSRO] successfully loaded")
 getKervanKey() # Just for testing purpose
