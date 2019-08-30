ed230ae6abb70fff4604f15f071fc3080b745415
JellyBitz <engelsquintero@gmail.com>
2019-08-30 15:00:24 -0300

    The config filter works independently now

--- xAutoConfig.py
+++ xAutoConfig.py
@@ -3,18 +3,20 @@ import shutil
 import os
 
 pName = 'xAutoConfig'
-pVersion = '0.2.1'
+pVersion = '0.2.2'
 pUrl = 'https://raw.githubusercontent.com/JellyBitz/phBot-xPlugins/master/xAutoConfig.py'
 
 # Called when the user successfully selects a character. No character data has been loaded yet.
 def joined_game():
-	# JSON config not found
+	# JSON config check
 	if not os.path.exists(get_config_path()):
 		# JSON default configs path
 		defaultConfig = get_config_dir()+"Default.json"
 		if os.path.exists(defaultConfig):
 			shutil.copyfile(defaultConfig,get_config_path())
 			log("Plugin: Default JSON loaded")
+	# db3 config check
+	if not os.path.exists(get_config_path().replace(".json",".db3")):
 		# db3 default filter path
 		defaultFilter = get_config_dir()+"Default.db3"
 		if os.path.exists(defaultFilter):
