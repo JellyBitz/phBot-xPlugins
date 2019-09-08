021435b1ffeffa42b29479a69b1b03e27e2c7e2c
JellyBitz <engelsquintero@gmail.com>
2019-09-08 16:50:33 -0300

    Added "capturetheflag" script command for CTF events Added anti afk system Changed some register types (requires testing)

--- xArena.py
+++ xArena.py
@@ -1,57 +1,112 @@
 from phBot import *
 
 pName = 'xArena'
-pVersion = '0.0.2'
+pVersion = '0.1.0'
 pUrl = 'https://raw.githubusercontent.com/JellyBitz/phBot-xPlugins/master/xArena.py'
 
 InBattleArena = False
+InCTF = False
+isPluginRegistering = False
 
 def handle_joymax(opcode, data):
+	global isPluginRegistering
 	if opcode == 0x34D2:
 		global InBattleArena
 		if data[0] == 0xFF:
 			result = data[1]
 			if result == 0x00:
 				log('Plugin: Successfully registered to arena')
-				stop_bot()
-				InBattleArena = True
+				if isPluginRegistering:
+					stop_bot()
 			elif result == 0x02:
 				log('Plugin: You already registered!')
-			elif result == 0x04:
-				log('Plugin: You may not register at this time')
-			elif result == 0x06:
-				log('Plugin: Match has been canceled duo to not enough players!')
-			elif result == 0x0B:
-				log("Plugin: Unable to register you're not in party")
-			elif result == 0x0D:
-				log("Plugin: You're not wearing the suit to register!")
+			else:
+				if result == 0x04:
+					log('Plugin: You may not register at this time')
+				elif result == 0x06:
+					log('Plugin: Match has been canceled, not enough players!')
+				elif result == 0x0B:
+					log("Plugin: Unable to register you're not in party")
+				elif result == 0x0D:
+					log("Plugin: You're not wearing the suit to register!")
+				isPluginRegistering = False
+		elif data[0] == 0x08:
+			InBattleArena = True
+			if isPluginRegistering:
+				AntiAFK()
 		elif data[0] == 0x09:
 			result = data[2]
 			coins = data[3]
 			log('Plugin: You have '+('lost' if result == 2 else 'won')+', you gained '+str(coins)+' coins!')
 			if InBattleArena:
 				InBattleArena = False
-				start_bot()
+				if isPluginRegistering:
+					isPluginRegistering = False
+					start_bot()
+	elif opcode == 0x34B1:
+		global InCTF
+		if data[0] == 0xFF:
+			result = data[1]
+			if result == 0x00:
+				log('Plugin: Successfully registered to CTF')
+				if isPluginRegistering:
+					stop_bot()
+			else:
+				if result == 0x11:
+					log('Plugin: You have won the match!')
+				elif result == 0x11:
+					log('Plugin: You have lost the match!')
+				elif result == 0x17:
+					log('Plugin: Match has ended in draw!')
+				elif result == 0x06:
+					log('Plugin: Match has been canceled, not enough players!')
+				elif result == 0x15:
+					log('Plugin: You are outside of the town!')
+				isPluginRegistering = False
+		elif data[0] == 0x08:
+			InCTF = True
+			if isPluginRegistering:
+				AntiAFK()
+		elif data[0] == 0x09:
+			result = data[2]
+			if InCTF:
+				InCTF = False
+				log('Plugin: Capture The Flag event finished')
+				if isPluginRegistering:
+					isPluginRegistering = False
+					start_bot()
 	return True
 
+# Gets the NPC unique ID if the specified name is found near
+def GetNPCUniqueID(name):
+	NPCs = get_npcs()
+	if NPCs:
+		name = name.lower()
+		for UniqueID, NPC in NPCs.items():
+			NPCName = NPC['name'].lower()
+			if name in NPCName:
+				return UniqueID
+	return 0
+
+# Register to the specified types "arena,type1,type2"
+# Type 1; Random, Party, Guild or Job
+# Type 2; Random, Score or Flag .. Or leave it empty if you are having issues (need test)
 def arena(arguments):
-	if len(arguments) < 3:
+	if len(arguments) < 2:
 		log('Plugin: Missing arena type in the script')
 		return 0
-
-	t1 = arguments[1].lower()
-	t2 = arguments[2].lower()
-	NPCID = 0
-
-	NPCs = get_npcs()
-	for UniqueID, NPC in NPCs.items():
-		if NPC['name'] == 'Arena Manager':
-			NPCID = UniqueID
-			break
+	
+	NPCID = GetNPCUniqueID('Arena Manager')
 
 	if NPCID == 0:
 		log('Plugin: "Arena Manager" is not near. Be sure to use the script command near to the NPC')
 	else:
+		# read register type
+		t1 = arguments[1].lower()
+		t2 = ''
+		if len(arguments) < 3:
+			t2 = arguments[2].lower()
+
 		p = bytearray()
 		# 1 = register; 2 = cancel
 		p.append(0x01)
@@ -65,23 +120,44 @@ def arena(arguments):
 			p.append(0x02)
 		elif t1 == 'job':
 			p.append(0x03)
-		elif t1 == 'ctf':
-			p.append(0x04)
 		else:
-			log('Plugin: Wrong Battle Arena type. Please be sure to select one: Random, Party, Guild, Job or CTF')
+			log('Plugin: Wrong Battle Arena type. Please be sure to select one: Random, Party, Guild or Job')
 			return 0
 
-		# 1 = Score; 2 = Flag;
-		if t2 == 'score':
+		# 0 = random, 1 = Score; 2 = Flag;
+		if t2 == '':
+			pass
+		elif t2 == 'random':
+			p.append(0x00)
+		elif t2 == 'score':
 			p.append(0x01)
 		elif t2 == 'flag':
 			p.append(0x02)
 		else:
-			log('Plugin: Wrong Battle Arena type. Please be sure to select one: Score or Flag')
+			log('Plugin: Wrong Battle Arena type. Please be sure to select one: Score, Flag or Random')
 			return 0
 
+		global isPluginRegistering
+		isPluginRegistering = True
 		inject_joymax(0x74D3, p, False)
 		return 500
 	return 0
+
+# Register to the captureflag event "capturetheflag"
+def capturetheflag(arguments):
+	NPCID = GetNPCUniqueID('So-Ok')
+	if NPCID == 0:
+		log('Plugin: NPC "So-Ok" is not near. Be sure to use the script command near to the NPC')
+	else:
+		p = bytearray()
+		inject_joymax(0x74B2, p, False)
+		return 500
+	return 0
+
+# The most dumb anti afk system working ever
+def AntiAFK():
+	if InBattleArena or InCTF:
+		inject_joymax(0x3091,b'\x01', False)
+		Timer(15.0, AntiAFK).start()
 
 log('Plugin: '+pName+' v'+pVersion+' succesfully loaded')
