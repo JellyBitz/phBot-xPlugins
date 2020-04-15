637e06b8e3197f5a05038a918b35171809fd6843
JellyBitz <engelsquintero@gmail.com>
2020-04-15 17:14:58 -0430

    Added Reload support Code maintenance

--- xChat.py
+++ xChat.py
@@ -6,18 +6,17 @@ import json
 import os
 
 pName = 'xChat'
-pVersion = '0.3.2'
+pVersion = '1.0.0'
 pUrl = 'https://raw.githubusercontent.com/JellyBitz/phBot-xPlugins/master/xChat.py'
 
-# Avoid issues
-inGame = False
+# ______________________________ Initializing ______________________________ #
 
 # Globals
-delayMsg = 10000 # delay between messages
-delayCounter = 0
+MESSAGES_DELAY = 10000 # delay between messages (ms)
+message_delay_counter = 0
 character_data = None
 
-# Initializing GUI
+# Graphic user interface
 gui = QtBind.init(__name__,pName)
 cbxMsg = QtBind.createCheckBox(gui, 'cbxMsg_clicked','Send message: ', 21, 13)
 cbxMsg_checked = False
@@ -48,6 +47,8 @@ cbxEvtChar_died = QtBind.createCheckBox(gui,'cbxLog_clicked','Character died',32
 cbxEvtPet_transport_died = QtBind.createCheckBox(gui,'cbxLog_clicked','Transport/Horse died',321,159)
 cbxEvtDrop_item = QtBind.createCheckBox(gui,'cbxLog_clicked','Item drop',321,178)
 cbxEvtDrop_rare = QtBind.createCheckBox(gui,'cbxLog_clicked','Rare drop',321,197)
+
+# ______________________________ Methods ______________________________ #
 
 # Return folder path
 def getPath():
@@ -80,100 +81,154 @@ def loadDefaultConfig():
 	QtBind.setChecked(gui,cbxEvtDrop_item,False)
 	QtBind.setChecked(gui,cbxEvtDrop_rare,False)
 
-# Load config if exists
-def loadConfig():
+# Loads all config previously saved
+def loadConfigs():
 	loadDefaultConfig()
-	if os.path.exists(getConfig()):
-		data = {}
-		with open(getConfig(),"r") as f:
-			data = json.load(f)
-		# Check to load config
-		if "cbxLogsInOne" in data and data["cbxLogsInOne"]:
-			QtBind.setChecked(gui,cbxLogsInOne,data["cbxLogsInOne"])
-		if "cbxLogAll" in data and data["cbxLogAll"]:
-			QtBind.setChecked(gui,cbxLogAll,data["cbxLogAll"])
-		if "cbxLogPrivate" in data and data["cbxLogPrivate"]:
-			QtBind.setChecked(gui,cbxLogPrivate,data["cbxLogPrivate"])
-		if "cbxLogParty" in data and data["cbxLogParty"]:
-			QtBind.setChecked(gui,cbxLogParty,data["cbxLogParty"])
-		if "cbxLogGuild" in data and data["cbxLogGuild"]:
-			QtBind.setChecked(gui,cbxLogGuild,data["cbxLogGuild"])
-		if "cbxLogUnion" in data and data["cbxLogUnion"]:
-			QtBind.setChecked(gui,cbxLogUnion,data["cbxLogUnion"])
-		if "cbxLogAcademy" in data and data["cbxLogAcademy"]:
-			QtBind.setChecked(gui,cbxLogAcademy,data["cbxLogAcademy"])
-		if "cbxLogStall" in data and data["cbxLogStall"]:
-			QtBind.setChecked(gui,cbxLogStall,data["cbxLogStall"])
-		if "cbxLogGlobal" in data and data["cbxLogGlobal"]:
-			QtBind.setChecked(gui,cbxLogGlobal,data["cbxLogGlobal"])
-		if "cbxLogNotice" in data and data["cbxLogNotice"]:
-			QtBind.setChecked(gui,cbxLogNotice,data["cbxLogNotice"])
-		if "cbxLogGM" in data and data["cbxLogGM"]:
-			QtBind.setChecked(gui,cbxLogGM,data["cbxLogGM"])
-		if "cbxLogUnknown" in data and data["cbxLogUnknown"]:
-			QtBind.setChecked(gui,cbxLogUnknown,data["cbxLogUnknown"])
-		if "cbxEvtSpawn_unique" in data and data["cbxEvtSpawn_unique"]:
-			QtBind.setChecked(gui,cbxEvtSpawn_unique,data["cbxEvtSpawn_unique"])
-		if "cbxEvtSpawn_hunter" in data and data["cbxEvtSpawn_hunter"]:
-			QtBind.setChecked(gui,cbxEvtSpawn_hunter,data["cbxEvtSpawn_hunter"])
-		if "cbxEvtSpawn_thief" in data and data["cbxEvtSpawn_thief"]:
-			QtBind.setChecked(gui,cbxEvtSpawn_thief,data["cbxEvtSpawn_thief"])
-		if "cbxEvtChar_attacked" in data and data["cbxEvtChar_attacked"]:
-			QtBind.setChecked(gui,cbxEvtChar_attacked,data["cbxEvtChar_attacked"])
-		if "cbxEvtChar_died" in data and data["cbxEvtChar_died"]:
-			QtBind.setChecked(gui,cbxEvtChar_died,data["cbxEvtChar_died"])
-		if "cbxEvtPet_transport_died" in data and data["cbxEvtPet_transport_died"]:
-			QtBind.setChecked(gui,cbxEvtPet_transport_died,data["cbxEvtPet_transport_died"])
-		if "cbxEvtDrop_item" in data and data["cbxEvtDrop_item"]:
-			QtBind.setChecked(gui,cbxEvtDrop_item,data["cbxEvtDrop_item"])
-		if "cbxEvtDrop_rare" in data and data["cbxEvtDrop_rare"]:
-			QtBind.setChecked(gui,cbxEvtDrop_rare,data["cbxEvtDrop_rare"])
+	if isJoined():
 
-# Save specific value at config
-def saveConfig(key,value):
-	if key:
-		data = {}
 		if os.path.exists(getConfig()):
+			data = {}
 			with open(getConfig(),"r") as f:
 				data = json.load(f)
-		data[key] = value
+
+			# Check to load config
+			if "cbxLogsInOne" in data and data["cbxLogsInOne"]:
+				QtBind.setChecked(gui,cbxLogsInOne,data["cbxLogsInOne"])
+			if "cbxLogAll" in data and data["cbxLogAll"]:
+				QtBind.setChecked(gui,cbxLogAll,data["cbxLogAll"])
+			if "cbxLogPrivate" in data and data["cbxLogPrivate"]:
+				QtBind.setChecked(gui,cbxLogPrivate,data["cbxLogPrivate"])
+			if "cbxLogParty" in data and data["cbxLogParty"]:
+				QtBind.setChecked(gui,cbxLogParty,data["cbxLogParty"])
+			if "cbxLogGuild" in data and data["cbxLogGuild"]:
+				QtBind.setChecked(gui,cbxLogGuild,data["cbxLogGuild"])
+			if "cbxLogUnion" in data and data["cbxLogUnion"]:
+				QtBind.setChecked(gui,cbxLogUnion,data["cbxLogUnion"])
+			if "cbxLogAcademy" in data and data["cbxLogAcademy"]:
+				QtBind.setChecked(gui,cbxLogAcademy,data["cbxLogAcademy"])
+			if "cbxLogStall" in data and data["cbxLogStall"]:
+				QtBind.setChecked(gui,cbxLogStall,data["cbxLogStall"])
+			if "cbxLogGlobal" in data and data["cbxLogGlobal"]:
+				QtBind.setChecked(gui,cbxLogGlobal,data["cbxLogGlobal"])
+			if "cbxLogNotice" in data and data["cbxLogNotice"]:
+				QtBind.setChecked(gui,cbxLogNotice,data["cbxLogNotice"])
+			if "cbxLogGM" in data and data["cbxLogGM"]:
+				QtBind.setChecked(gui,cbxLogGM,data["cbxLogGM"])
+			if "cbxLogUnknown" in data and data["cbxLogUnknown"]:
+				QtBind.setChecked(gui,cbxLogUnknown,data["cbxLogUnknown"])
+			if "cbxEvtSpawn_unique" in data and data["cbxEvtSpawn_unique"]:
+				QtBind.setChecked(gui,cbxEvtSpawn_unique,data["cbxEvtSpawn_unique"])
+			if "cbxEvtSpawn_hunter" in data and data["cbxEvtSpawn_hunter"]:
+				QtBind.setChecked(gui,cbxEvtSpawn_hunter,data["cbxEvtSpawn_hunter"])
+			if "cbxEvtSpawn_thief" in data and data["cbxEvtSpawn_thief"]:
+				QtBind.setChecked(gui,cbxEvtSpawn_thief,data["cbxEvtSpawn_thief"])
+			if "cbxEvtChar_attacked" in data and data["cbxEvtChar_attacked"]:
+				QtBind.setChecked(gui,cbxEvtChar_attacked,data["cbxEvtChar_attacked"])
+			if "cbxEvtChar_died" in data and data["cbxEvtChar_died"]:
+				QtBind.setChecked(gui,cbxEvtChar_died,data["cbxEvtChar_died"])
+			if "cbxEvtPet_transport_died" in data and data["cbxEvtPet_transport_died"]:
+				QtBind.setChecked(gui,cbxEvtPet_transport_died,data["cbxEvtPet_transport_died"])
+			if "cbxEvtDrop_item" in data and data["cbxEvtDrop_item"]:
+				QtBind.setChecked(gui,cbxEvtDrop_item,data["cbxEvtDrop_item"])
+			if "cbxEvtDrop_rare" in data and data["cbxEvtDrop_rare"]:
+				QtBind.setChecked(gui,cbxEvtDrop_rare,data["cbxEvtDrop_rare"])
+
+# Save all config
+def saveConfigs():
+	# Save if data has been loaded
+	if isJoined():
+		# Save all data
+		data = {}
+		data["cbxLogsInOne"] = QtBind.isChecked(gui,cbxLogsInOne)
+		data["cbxLogAll"] = QtBind.isChecked(gui,cbxLogAll)
+		data["cbxLogPrivate"] = QtBind.isChecked(gui,cbxLogPrivate)
+		data["cbxLogParty"] = QtBind.isChecked(gui,cbxLogParty)
+		data["cbxLogGuild"] = QtBind.isChecked(gui,cbxLogGuild)
+		data["cbxLogUnion"] = QtBind.isChecked(gui,cbxLogUnion)
+		data["cbxLogAcademy"] = QtBind.isChecked(gui,cbxLogAcademy)
+		data["cbxLogStall"] = QtBind.isChecked(gui,cbxLogStall)
+		data["cbxLogGlobal"] = QtBind.isChecked(gui,cbxLogGlobal)
+		data["cbxLogNotice"] = QtBind.isChecked(gui,cbxLogNotice)
+		data["cbxLogGM"] = QtBind.isChecked(gui,cbxLogGM)
+		data["cbxLogUnknown"] = QtBind.isChecked(gui,cbxLogUnknown)
+		data["cbxEvtSpawn_unique"] = QtBind.isChecked(gui,cbxEvtSpawn_unique)
+		data["cbxEvtSpawn_hunter"] = QtBind.isChecked(gui,cbxEvtSpawn_hunter)
+		data["cbxEvtSpawn_thief"] = QtBind.isChecked(gui,cbxEvtSpawn_thief)
+		data["cbxEvtChar_attacked"] = QtBind.isChecked(gui,cbxEvtChar_attacked)
+		data["cbxEvtChar_died"] = QtBind.isChecked(gui,cbxEvtChar_died)
+		data["cbxEvtPet_transport_died"] = QtBind.isChecked(gui,cbxEvtPet_transport_died)
+		data["cbxEvtDrop_item"] = QtBind.isChecked(gui,cbxEvtDrop_item)
+		data["cbxEvtDrop_rare"] = QtBind.isChecked(gui,cbxEvtDrop_rare)
+
+		# Overrides
 		with open(getConfig(),"w") as f:
 			f.write(json.dumps(data, indent=4, sort_keys=True))
+		log("Plugin: "+pName+" configs has been saved")
 
-# Called when the bot successfully connects to the game server
-def connected():
-	global inGame
-	inGame = False
-
-# Called when the character enters the game world
-def joined_game():
-	global inGame,character_data
-	inGame = True
+# Check if character is ingame
+def isJoined():
+	global character_data
 	character_data = get_character_data()
-	loadConfig()
+	return character_data and "name" in character_data and character_data["name"]
 
 # Just saving everything everytime (slower method)
 def cbxLog_clicked(checked):
-	saveConfig("cbxLogsInOne",QtBind.isChecked(gui,cbxLogsInOne))
-	saveConfig("cbxLogAll",QtBind.isChecked(gui,cbxLogAll))
-	saveConfig("cbxLogPrivate",QtBind.isChecked(gui,cbxLogPrivate))
-	saveConfig("cbxLogParty",QtBind.isChecked(gui,cbxLogParty))
-	saveConfig("cbxLogGuild",QtBind.isChecked(gui,cbxLogGuild))
-	saveConfig("cbxLogUnion",QtBind.isChecked(gui,cbxLogUnion))
-	saveConfig("cbxLogAcademy",QtBind.isChecked(gui,cbxLogAcademy))
-	saveConfig("cbxLogStall",QtBind.isChecked(gui,cbxLogStall))
-	saveConfig("cbxLogGlobal",QtBind.isChecked(gui,cbxLogGlobal))
-	saveConfig("cbxLogNotice",QtBind.isChecked(gui,cbxLogNotice))
-	saveConfig("cbxLogGM",QtBind.isChecked(gui,cbxLogGM))
-	saveConfig("cbxLogUnknown",QtBind.isChecked(gui,cbxLogUnknown))
-	saveConfig("cbxEvtSpawn_unique",QtBind.isChecked(gui,cbxEvtSpawn_unique))
-	saveConfig("cbxEvtSpawn_hunter",QtBind.isChecked(gui,cbxEvtSpawn_hunter))
-	saveConfig("cbxEvtSpawn_thief",QtBind.isChecked(gui,cbxEvtSpawn_thief))
-	saveConfig("cbxEvtChar_attacked",QtBind.isChecked(gui,cbxEvtChar_attacked))
-	saveConfig("cbxEvtChar_died",QtBind.isChecked(gui,cbxEvtChar_died))
-	saveConfig("cbxEvtPet_transport_died",QtBind.isChecked(gui,cbxEvtPet_transport_died))
-	saveConfig("cbxEvtDrop_item",QtBind.isChecked(gui,cbxEvtDrop_item))
-	saveConfig("cbxEvtDrop_rare",QtBind.isChecked(gui,cbxEvtDrop_rare))
+	saveConfigs()
+
+# Check to start sending messages
+def cbxMsg_clicked(checked):
+	global cbxMsg_checked
+	cbxMsg_checked = checked
+	if checked:
+		# restart spamer counter
+		global message_delay_counter
+		message_delay_counter = 0
+		QtBind.setText(gui,lblCounter,"0")
+		log("Plugin: Starting spam")
+	else:
+		log("Plugin: Stopping spam")
+
+# ______________________________ Events ______________________________ #
+
+# Send message, even through script. Ex. "chat,All,Hello World!" or "chat,private,JellyBitz,Hi!"
+def chat(args):
+	# check arguments length and empty message
+	if (len(args) >= 3 and len(args[2]) > 0):
+		success = False
+		type = args[1].lower()
+		if type == "all":
+			success = phBotChat.All(args[2])
+		elif type == "private":
+			success = phBotChat.Private(args[2],args[3])
+		elif type == "party":
+			success = phBotChat.Party(args[2])
+		elif type == "guild":
+			success = phBotChat.Guild(args[2])
+		elif type == "union":
+			success = phBotChat.Union(args[2])
+		elif type == "note":
+			success = phBotChat.Note(args[2],args[3])
+		elif type == "stall":
+			success = phBotChat.Stall(args[2])
+		if success:
+			log("Plugin: Message sent successfully ("+pName+")")
+
+# Save message to the log.txt "logline,text"
+def logline(args):
+	if len(args) == 2:
+		path = "log.txt" if QtBind.isChecked(gui,cbxLogsInOne) else character_data["server"]+"_"+character_data["name"]+"_log.txt"
+		date = strftime("%d/%m %I:%M:%S %p", localtime())
+		with open(getPath()+path,"a+") as f:
+			f.write(date+" > "+args[1])
+
+# Called when the bot successfully connects to the game server
+def connected():
+	global character_data
+	character_data = None
+
+# Called when the character enters the game world
+def joined_game():
+	loadConfig()
 
 # All chat messages received are sent to this function
 def handle_chat(t,player,msg):
@@ -226,63 +281,26 @@ def handle_event(t, data):
 	elif t == 7 and QtBind.isChecked(gui,cbxEvtChar_died):
 		logline(["","[Character][Died]"])
 
-# Save message to the log.txt "logline,text"
-def logline(args):
-	if len(args) == 2:
-		path = "log.txt" if QtBind.isChecked(gui,cbxLogsInOne) else character_data["server"]+"_"+character_data["name"]+"_log.txt"
-		date = strftime("%d/%m %I:%M:%S %p", localtime())
-		with open(getPath()+path,"a+") as f:
-			f.write(date+" > "+args[1])
-
-# Check to start sending messages
-def cbxMsg_clicked(checked):
-	global cbxMsg_checked
-	cbxMsg_checked = checked
-	if checked:
-		# restart spamer counter
-		global delayCounter
-		delayCounter = 0
-		QtBind.setText(gui,lblCounter,"0")
-		log("Plugin: Starting spam")
-	else:
-		log("Plugin: Stopping spam")
-
-# Send message, even through script. Ex. "chat,All,Hello World!" or "chat,private,JellyBitz,Hi!"
-def chat(args):
-	# check arguments length and empty message
-	if (len(args) >= 3 and len(args[2]) > 0):
-		success = False
-		type = args[1].lower()
-		if type == "all":
-			success = phBotChat.All(args[2])
-		elif type == "private":
-			success = phBotChat.Private(args[2],args[3])
-		elif type == "party":
-			success = phBotChat.Party(args[2])
-		elif type == "guild":
-			success = phBotChat.Guild(args[2])
-		elif type == "union":
-			success = phBotChat.Union(args[2])
-		elif type == "note":
-			success = phBotChat.Note(args[2],args[3])
-		elif type == "stall":
-			success = phBotChat.Stall(args[2])
-		if success:
-			log("Plugin: Message sent successfully ("+pName+")")
-
-# Called every 500ms.
+# Called every 500ms
 def event_loop():
-	if inGame:
+	if character_data:
 		if cbxMsg_checked:
-			global delayCounter
-			if delayCounter%delayMsg == 0:
-				chat(['chat','All',QtBind.text(gui,tbxMsg)])
-				QtBind.setText(gui,lblCounter,str(int(QtBind.text(gui,lblCounter))+1))
-			delayCounter += 500
+			global message_delay_counter
+			message_delay_counter += 500
+			if message_delay_counter >= MESSAGES_DELAY:
+				message_delay_counter = 0
+				message = QtBind.text(gui,tbxMsg)
+				if message:
+					phBotChat.All(QtBind.text(gui,tbxMsg))
+					QtBind.setText(gui,lblCounter,str(int(QtBind.text(gui,lblCounter))+1))
 
-# Plugin load success
+# Plugin loaded
 log('Plugin: '+pName+' v'+pVersion+' successfully loaded')
-# Creating configs folder
-if not os.path.exists(getPath()):
+
+if os.path.exists(getPath()):
+	# Adding RELOAD plugin support
+	loadConfigs()
+else:
+	# Creating configs folder
 	os.makedirs(getPath())
 	log('Plugin: '+pName+' folder has been created')
