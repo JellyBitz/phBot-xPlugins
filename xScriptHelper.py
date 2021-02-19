f121b1ebfabc48704d68cf6b7c195d8fdeb05adf
Engels Quintero <engelsquintero@gmail.com>
2021-02-19 05:44:39 +0400

    New plugin! To add useful commands to script

---
+++ xScriptHelper.py
@@ -0,0 +1,323 @@
+from phBot import *
+from threading import Timer
+from time import sleep
+import sqlite3
+import struct
+
+pName = 'xScriptHelper'
+pVersion = '1.2.1'
+pUrl = 'https://raw.githubusercontent.com/JellyBitz/phBot-xPlugins/master/xScriptHelper.py'
+
+# Script Commands :
+# - Dismantle items from inventory
+# dismantle,ItemName?
+# - Take items from character storage but keeping the space on inventory empty
+# DoStorageTakeLimit,emptySlots
+
+# ______________________________ Initializing ______________________________ #
+
+DISMANTLE_MAX_ATTEMPTS = 3
+
+# globals
+StorageData = None
+CharacterData = None
+
+# dismantling process
+DismantlingCurrentSlot = 0
+DismantlingAttempts = 0
+DismantlingBlacklisted = []
+DismantlingErrorCode = 0
+
+# ______________________________ Methods ______________________________ #
+
+def Inject_DismantleItem(ItemSlot):
+	p = struct.pack('BB',1,ItemSlot)
+	inject_joymax(0x7157,p,False)
+
+def Inject_SelectEntity(EntityID):
+	inject_joymax(0x7045,struct.pack('<I',EntityID),False)
+
+def Inject_RequestStorageData(EntityID):
+	p = struct.pack('<IB',EntityID,0)
+	inject_joymax(0x703C,p,False)
+
+def Inject_NpcTalk(EntityID,TalkID):
+	p = struct.pack('<IB',EntityID,TalkID)
+	inject_joymax(0x7046,p,False)
+
+def Inject_InventoryStorageMovement(MovementType,SlotInitial,SlotFinal,EntityID):
+	p = struct.pack('B',MovementType)
+	p += struct.pack('B',SlotInitial)
+	p += struct.pack('B',SlotFinal)
+	p += struct.pack('<I',EntityID)
+	inject_joymax(0x7034,p,False)
+
+def Inject_ExitNpc(EntityID):
+	inject_joymax(0x704B,struct.pack('<I',EntityID),False)
+
+# Dismantle items if some item is found
+def DismantleItem(ItemName=None):
+	global DismantlingErrorCode
+	if DismantlingErrorCode:
+		if DismantlingErrorCode == 22536:
+			log("Plugin: Not enough inventory space to continue dismantling!")
+		elif DismantlingErrorCode == 22535:
+			log("Plugin: Not enough Destroyer Rondo's to continue dismantling!")
+		else:
+			log("Plugin: Error code ("+str(DismantlingErrorCode)+") to continue dismantling!")
+		DismantlingErrorCode = 0
+		start_bot()
+		return
+	inventory = get_inventory()
+	items = inventory['items']
+	itemFound = False
+	# Checkk items from inventory
+	for slot, item in enumerate(items):
+		if item and slot > 12:
+			itemData = get_item(item['model'])
+			# Make sure item is equipable
+			if itemData['tid1'] != 1:
+				continue
+			# Make sure item is not an avatar or job item
+			if itemData['tid2'] == 7 or itemData['tid2'] == 13:
+				continue
+			# Check if it's dismantling by name 
+			if ItemName and ItemName != item['name']:
+				continue
+
+			# if at least one item has been found, stop bot!
+			itemFound = True
+			stop_bot()
+			# Check if this slot cannot be dismantled
+			if slot in DismantlingBlacklisted:
+				continue
+			# Check dismantling attempts
+			global DismantlingCurrentSlot
+			global DismantlingAttempts
+			if slot == DismantlingCurrentSlot:
+				DismantlingAttempts+=1
+			else:
+				DismantlingAttempts=0
+			if DismantlingAttempts == DISMANTLE_MAX_ATTEMPTS:
+				log('Plugin: Skiping "'+item['name']+'", too many dismantling attempts!')
+				DismantlingBlacklisted.append(slot)
+				continue
+			# Try dismantle it
+			DismantlingCurrentSlot = slot
+			log('Plugin: Trying to dismantle "'+item['name']+'"...')
+			Inject_DismantleItem(slot)
+			# waits a little bit for item to be dismantled
+			Timer(3,DismantleItem,[ItemName]).start()
+			break
+	# restart bot if nothing can be found
+	if not itemFound:
+		log("Plugin: Dismantling finished!")
+		start_bot()
+
+# Count slots availables on inventory
+def CountInventoryEmptySlots():
+	items = get_inventory()['items']
+	count = 0
+	# check items between intervals
+	for slot, item in enumerate(items):
+		if slot >= 13:
+			if not item:
+				count+=1
+	return count
+
+# Finds an empty slot, returns -1 if inventory is full
+def GetInventoryEmptySlot():
+	items = get_inventory()['items']
+	# check the first empty
+	for slot, item in enumerate(items):
+		if slot >= 13:
+			if not item:
+				return slot
+	return -1
+
+# Find npc by name or servername through lambda expression and return his information
+def FindNpcByExpression(_lambda):
+	npcs = get_npcs()
+	if npcs:
+		for uid, npc in npcs.items():
+			# Search by lambda
+			if _lambda(npc['name'],npc['servername']):
+				# Save unique id
+				npc['entity_id'] = uid
+				return npc
+	return None
+
+# Executes all the process to open, take items, and close storage
+def DoStorageTakeLimitTask(NpcInfo,KeepInventoryEmptySlots):
+	entityID = NpcInfo['entity_id']
+	# Check if storage has been loaded and try to open it
+	if not StorageData:
+		log("Plugin: Loading Storage...")
+		# Try to select NPC
+		Inject_SelectEntity(entityID)
+		sleep(2)
+		# Load data
+		Inject_RequestStorageData(entityID)
+		sleep(2)
+		# Close it
+		Inject_ExitNpc(entityID)
+		sleep(2)
+	# Check if some item can be taken
+	conn = GetFilterConnection()
+	item = GetTakeableStorageItem(conn)
+	if not item:
+		# Stop looking for it
+		conn.close()
+		return
+	# stop bot script
+	stop_bot()
+	sleep(1)
+	# open npc to start the process again since phBot close it
+	log("Plugin: Selecting NPC...")
+	Inject_SelectEntity(entityID)
+	sleep(2)
+	log("Plugin: Entering NPC...")
+	Inject_NpcTalk(entityID,3)
+	sleep(2)
+	# double check infinite loop
+	takeCurrentSlot = 0 
+	takeAttempts = 0
+	takeAttemptsMax = 3
+	# Try to take item
+	while item:
+		emptySlot = GetInventoryEmptySlot()
+		if not emptySlot:
+			# Inventory is full
+			break
+		# Track a max attempts on the next take
+		takeCurrentSlot = item['slot']
+		# Move item from storage to empty slot
+		log('Plugin: Taking item "'+item['name']+'"...')
+		Inject_InventoryStorageMovement(3,item['slot'],emptySlot,entityID)
+		# Wait a little
+		sleep(2)
+		# Check if empty slots has been reached
+		if CountInventoryEmptySlots() <= KeepInventoryEmptySlots:
+			break
+		# Search for another item
+		item = GetTakeableStorageItem(conn)
+		if not item:
+			break
+		# Track max attempts
+		if takeCurrentSlot == item['slot']:
+			takeAttempts+=1
+		else:
+			takeAttempts=0
+		if takeAttempts == takeAttemptsMax:
+			log("Plugin: Unnexpected error, too many attempts to take the item. Aborting...")
+			break
+	# All done
+	conn.close()
+	log("Plugin: Exiting NPC...")
+	Inject_ExitNpc(entityID)
+	sleep(1)
+	# restart bot
+	start_bot()
+
+# Create a database connection to config filter
+def GetFilterConnection():
+	# Path to the filter database
+	path = get_config_dir()+CharacterData['server']+'_'+CharacterData['name']+'.db3'
+	# Connect to db3
+	return sqlite3.connect(path)
+
+# Check if character is ingame
+def IsJoined():
+	global CharacterData
+	CharacterData = get_character_data()
+	if not (CharacterData and "name" in CharacterData and CharacterData["name"]):
+		CharacterData = None
+	return CharacterData
+
+# Search for an takeable item from storage by using the filter database
+def GetTakeableStorageItem(FilterConnection):
+	global StorageData
+	StorageData = get_storage()
+	if not StorageData:
+		return None
+	# Check all items and compare to database
+	items = StorageData['items']
+	c = FilterConnection.cursor()
+	for slot, item in enumerate(items):
+		# avoid empty slots
+		if item:
+			# Check if can be taken
+			if c.execute('SELECT EXISTS(SELECT 1 FROM pickfilter WHERE id=? AND takestorage=1 LIMIT 1)',(item['model'],)).fetchone()[0]:
+				item['slot'] = slot
+				return item
+	return None
+
+# ______________________________ Events ______________________________ #
+
+# Try to dismantle items on inventory
+def dismantle(args):
+	# reset some vars
+	DismantlingBlacklisted.clear()
+	# check params
+	if len(args) > 1 and args[1]:
+		# dismantle by item name
+		DismantleItem(args[1])
+	else:
+		# dismantle all
+		DismantleItem()
+	# All done
+	return 0
+
+# Takes items from storage
+def DoStorageTakeLimit(args):
+	# check params
+	keepEmptySlots = 0
+	if len(args) > 1:
+		keepEmptySlots = int(args[1])
+	# check if there is space available
+	emptySlots = CountInventoryEmptySlots()
+	if emptySlots <= keepEmptySlots:
+		# cancel process, no space available
+		return 0
+	# Check if storage is near
+	npcInfo = FindNpcByExpression(lambda n,s: '_WAREHOUSE' in s)
+	if npcInfo:
+		# Avoid lock bot thread
+		Timer(0.001,DoStorageTakeLimitTask,[npcInfo,keepEmptySlots]).start()
+		# Wait a little bit while opening NPC and check items
+		return 7000
+	else:
+		log("Plugin: Storage is not near!")
+		return 0
+
+# Called when the character enters the game world
+def joined_game():
+	global CharacterData
+	CharacterData = get_character_data()
+
+# Called after being teleported
+def teleported():
+	global StorageData
+	StorageData = None
+
+# All packets received from game server will be passed to this function
+# Returning True will keep the packet and False will not forward it to the game client
+def handle_joymax(opcode, data):
+	# SERVER_ALCHEMY_DISMANTLE_RESPONSE
+	if opcode == 0xB157:
+		# check failure
+		if data[0] != 1:
+			global DismantlingErrorCode
+			DismantlingErrorCode = struct.unpack_from('<H',data,1)[0]
+	# SERVER_STORAGE_DATA_END
+	elif opcode == 0x3048:
+		global StorageData
+		StorageData = get_storage()
+	return True
+
+# Plugin loaded
+log('Plugin: '+pName+' v'+pVersion+' succesfully loaded')
+
+# Check if storage has been loaded
+if IsJoined():
+	StorageData = get_storage()
